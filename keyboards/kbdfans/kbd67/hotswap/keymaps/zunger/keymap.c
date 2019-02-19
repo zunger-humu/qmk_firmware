8d4b3d65fe35704fb929a6cadef86538d45d33f3
Yonatan Zunger <zunger@humu.com>
2019-02-19 11:01:01 -0800

    Make FN+ESC grave, to make backticks easier; move screen lock to fn+flower.

--- keyboards/kbdfans/kbd67/hotswap/keymaps/zunger/keymap.c
+++ keyboards/kbdfans/kbd67/hotswap/keymaps/zunger/keymap.c
@@ -19,6 +19,15 @@ enum custom_keycodes {
   // Use this instead of RALT, so we can use it to switch layers but not trigger other alt-related
   // behavior (like GRAVE_ESC_ALT_OVERRIDE).
   MAGIC = SAFE_RANGE,
+  // The physical ESC key maps to grave-escape in the base layer, and to "smart reset" in the fn
+  // layer. This way:
+  //   ESC = Escape
+  //   Shift-ESC = ~
+  //   Cmd-ESC = Cmd-backtick
+  //   Cmd-Opt-ESC = Cmd-Opt-ESC
+  //   Fn-ESC = AltGr-ESC = backtick
+  //   Fn-LShift-Esc = Reset
+  SMART_RESET,
 };
 
 enum layers_keymap {
@@ -48,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [_QWERTY] = LAYOUT(
-  KC_GESC, KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BRK,  KC_HOME, \
+  KC_GESC, KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,  KC_HOME, \
   KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_END,  \
   KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP, \
   KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   KC_PGDN, \
@@ -56,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Keymap Fn Layer. Blank keys are deliberately NO, not TRNS.
    * ,----------------------------------------------------------------.
-   * |Rst |F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|F13|PSc|PAUS|
+   * |Rst |F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Psc|Brk|PAUS|
    * |----------------------------------------------------------------|
    * |Debug|   |   |   |   |   |   |   |   |   |   |   |   |     |    |
    * |----------------------------------------------------------------|
@@ -68,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [_FUNCTION] = LAYOUT(
-   RESET,   KC_F1,   KC_F2,       KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10, KC_F11, KC_F12, KC_F13, KC_PSCR,  KC_MPLY, \
+   SMART_RESET,  KC_F1,   KC_F2,       KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10, KC_F11, KC_F12, KC_PSCR, KC_BRK, KC_MPLY, \
    DEBUG,   KC_NO,   KC_NO,       KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,            _______,   \
    _______, KC_NO,   KC__VOLUP,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,          KC_NO,            KC_NO,   \
    _______, KC_MRWD, KC__VOLDOWN, KC_MFFD, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,                  KC_BTN1, KC_MS_U, KC_BTN2, \
@@ -141,11 +150,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     shifted = record->event.pressed;
   } else if (keycode == MAGIC) {
     magic = record->event.pressed;
-  } else if (keycode == RESET) {
+  } else if (keycode == SMART_RESET) {
     // Safe reset: Only actually let this keycode through if shift is held as well. Since there's no
     // right-shift in the function layer, this means that reset is Fn+LShift+Esc, something you're
     // not likely to hit by accident. (Especially since AltGr+Esc is backtick!)
-    return (record->event.pressed && shifted);
+    if (record->event.pressed && shifted) {
+      reset_keyboard();
+      return false;
+    } else if (record->event.pressed) {
+      register_code(KC_GRV);
+    } else {
+      unregister_code(KC_GRV);
+    }
   }
 
   // Update the layer.
